84b3bfd148183722ed0f0fa05f75573bda54a8c7
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2023-11-25 19:50:11 +0100

    Refactoring.

--- src/linux.mk
+++ src/linux.mk
@@ -1,4 +1,8 @@
-SRCDIRS = `find . -type d -maxdepth 1 \
+# find . -type d -maxdepth 1 -not -name CVS -not -name globus-sge -not -name sun_sge -not -name sge -not -name .
+# find: warning: you have specified the -maxdepth option after a non-option argument -type, but options are not 
+# positional (-maxdepth affects tests specified before it as well as those specified after it).  Please specify 
+# options before other arguments.
+SRCDIRS = `find . -maxdepth 1 -type d \
 	-not -name CVS \
 	-not -name globus-sge \
 	-not -name sun_sge \
